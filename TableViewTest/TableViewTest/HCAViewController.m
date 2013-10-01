53b4e2dac3e04d583848f35b791cc70eb9173e0f
Brian Coyner <briancoyner@mac.com>
2013-10-01 13:10:02 -0500

    minor updates

--- TableViewTest/TableViewTest/HCAViewController.m
+++ TableViewTest/TableViewTest/HCAViewController.m
@@ -29,7 +29,7 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[self demoTableViewCellIdentifier ]];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[self demoTableViewCellIdentifier] forIndexPath:indexPath];
     [cell setAccessoryType:UITableViewCellAccessoryDisclosureIndicator];
     [[cell textLabel] setText:@"UITableView Memory Issue"];
 
